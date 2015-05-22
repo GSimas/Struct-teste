34ccdf63667f6c8ff005ea2c10a29ae01909b947
User <s>
2015-05-22 17:28:37 -0300

    V2.1
    mostra instância

--- struct-teste/main.c
+++ struct-teste/main.c
@@ -21,13 +21,17 @@ struct Circulo{
 void leInstancias(struct Ponto2D *p, struct Circulo *c){
 	printf("Digite o valor da instancia1 para o ponto");
 	printf("Digite o valor da instancia2 para o circulo");
-	scanf("%f",&p);
-};
+	scanf("%f",&p)};
+
+void mostraInstancias(const struct Ponto2D *p, const struct Circulo *c){
+
+}
 
 int main()
 {
 	struct Ponto2D ponto1;
 	struct Circulo circ1;
+	leInstancias(ponto1,circ1);
 
 return 0;
 }
